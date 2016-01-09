b913a6d529c2f5b6fb87fa5a7858004153712b10
Aron <ender4037@gmail.com>
2016-01-08 23:10:36 -0700

    Simple beginings

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>NerdAron</title>
+	</head>
+
+	<body>
+		<header>
+			<p>The Header!</p>
+		</header>
+
+		<div id="main">
+			<p>This is the main body!!!</p>
+		</div>
+
+		<footer>
+			<p>The footer</p>
+		</footer>
+
+	</body>
+</html>
